2e2c45b11d8c5a2a1d9cb73a10097e5701e0cfcc
adrian5517 <juelboncodin@gmail.com>
2025-06-07 12:19:56 +0800

    Adding DarkMode and Stars Decoration for Background

--- myPortfolio/src/pages/Home.jsx
+++ myPortfolio/src/pages/Home.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { ThemeToggle } from '../components/ThemeToggle'
+import { StarBackground } from '@/components/StarBackground'
 
 export default function Home() {
   return (
-    <div>
-      Home Page
+    <div className='min-h-screen bg-background text-foreground overflow-hidden'>
+      {/* Theme Toggle */}
+      <ThemeToggle />
+      {/* Background Effects */}
+      <StarBackground/>
+      
+      {/* NavBar */}
+
+      {/* Main Content */}
+
+      {/* Footer Section */}
+      
     </div>
   )
 }
